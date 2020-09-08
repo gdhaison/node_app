1630c442eab902a1851c095c830f683037d6a6c4
hoaronal <hoadoan@tamdongtam.vn>
2020-09-08 23:20:55 +0700

    refactor code

---
+++ src/models/dto/UserInfoRequest.ts
@@ -0,0 +1,13 @@
+import {IsEmail, IsNumber, IsPhoneNumber, IsString, Matches} from "class-validator";
+import {JsonProperty} from "json-object-mapper";
+
+export class UserInfoRequest {
+    @IsEmail()
+    email!: string;
+
+    @IsString()
+    name!: string;
+
+    @IsString()
+    address!: string;
+}
