ab9aba4dfb1a6b582e4b1d851641b0dea2954d3b
gdhaison <vuhaisonhb456@gmail.som>
2020-09-26 15:11:11 +0700

    fix_rexgrimes

--- src/repositories/LwFoodRepository.ts
+++ src/repositories/LwFoodRepository.ts
@@ -24,23 +24,26 @@ export class LwFoodRepository extends Repository<LwFood> {
         Promise<{ total: number; data: any; nextPage: boolean; limit: number; from: number; page: number; to: number }> {
         const skippedItems = (page - 1) * limit;
         const select_query =
-            "SELECT DISTINCT lf.id, lf.image, lf.name, lf.calo,lf.description," +
-            "(select lfs1.star as user_star from lw_food_star lfs1  where lfs1.res_partner_id = " + user_id +" and lfs1.food_id = lf.id)," +
-            "(SELECT COUNT(1) FROM lw_food_star lfs WHERE lfs.res_partner_id = "+ user_id +" AND lfs.like_flag = 1 " +
-            "AND lfs.food_id = lf.id) AS heart,(SELECT round(AVG(lfs.star)) FROM lw_food_star lfs " +
-            "WHERE lfs.food_id = lf.id) AS star ";
+            `SELECT DISTINCT lf.id, lf.image, lf.name, lf.calo,lf.description,
+            (select lfs1.star as user_star from lw_food_star lfs1  where lfs1.res_partner_id = ${user_id} and lfs1.food_id = lf.id),
+            (SELECT COUNT(1) FROM lw_food_star lfs WHERE lfs.res_partner_id = ${user_id} AND lfs.like_flag = 1 
+            AND lfs.food_id = lf.id)::INTEGER AS heart,(SELECT round(AVG(lfs.star)) FROM lw_food_star lfs 
+            WHERE lfs.food_id = lf.id)::INTEGER AS star `;
 
         const query =
-            "FROM lw_food lf LEFT JOIN lw_food_star lfs ON lfs.food_id = lf.id inner join lw_food_category lfc " +
-            "on lfc.food_id = lf.id inner join lw_category lc on lc.code = lfc.category_code ";
-        let condition = "WHERE  lc.code = '"+ category +"'";
+            `FROM lw_food lf LEFT JOIN lw_food_star lfs ON lfs.food_id = lf.id inner join lw_food_category lfc 
+            on lfc.food_id = lf.id inner join lw_category lc on lc.code = lfc.category_code `;
+        let condition = "";
 
-        if (search_text) {
-            condition = "WHERE lf.name LIKE '%"+ search_text +"%' and lc.code = '"+ category +"'";
+        if (category == "") {
+            condition = `WHERE lf.name LIKE '%${search_text}%'`;
         }
-        let count = await this.entityManager.query("SELECT COUNT(DISTINCT(lf.id)) " + query + condition);
+        else {
+            condition = `WHERE lf.name LIKE '%${search_text}%'and lc.code = '${category}' `;
+        }
+        let count = await this.entityManager.query(`SELECT COUNT(DISTINCT(lf.id)) ${query} ${condition}`);
         if(category == "food-like") {
-            count = await this.entityManager.query("SELECT COUNT(DISTINCT(lf.id)) " + query + condition + "AND lfc.partner_id = " + user_id);
+            count = await this.entityManager.query(`SELECT COUNT(DISTINCT(lf.id)) ${query} ${condition} AND lfc.partner_id = ${user_id}`);
         }
 
         const total = parseInt(count[0]["count"]);
@@ -78,9 +81,10 @@ export class LwFoodRepository extends Repository<LwFood> {
 
     async getById(id: number, partnerId: number): Promise<any> {
         // const data = this.entityManager.query("SELECT lf.name FROM lw_food AS lf WHERE lf.name = $1 AND lf.lastName = $2", ["John", "Doe"]);
-        const data = await this.entityManager.query("SELECT lf.id, lf.image, lf.name, lf.calo, lf.total_like as heart, lfs.like_flag as is_like, " +
-            "lfs.star as user_star, (select round(avg(lfs1.star)) from lw_food_star lfs1 where lfs1.food_id = lf.id) as star_avg" +
-            ", lf.description FROM lw_food AS lf left join lw_food_star lfs on lfs.food_id = lf.id WHERE lf.id = $1 and lfs.res_partner_id = $2", [id, partnerId]);
+        const data = await this.entityManager.query(`SELECT lf.id, lf.image, lf.name, lf.calo, lfs.like_flag as is_like, 
+            (SELECT COUNT(1) FROM lw_food_star lfs2 WHERE lfs2.res_partner_id = ${partnerId} AND lfs2.like_flag = 1 AND lfs2.food_id = lf.id)::INTEGER AS heart, 
+            lfs.star as user_star, (select round(avg(lfs1.star)) from lw_food_star lfs1 where lfs1.food_id = lf.id)::INTEGER as star_avg 
+            , lf.description FROM lw_food AS lf left join lw_food_star lfs on lfs.food_id = lf.id WHERE lf.id = ${id} and lfs.res_partner_id = ${partnerId}`);
         if (Array.isArray(data) && data.length)
             return data[0];
         throw new FoodNotFoundError(ErrorCode.FOOD_NOT_FOUND);
@@ -170,15 +174,15 @@ export class LwFoodRepository extends Repository<LwFood> {
 
         const skippedItems = (page - 1) * limit;
         const select_query =
-            "SELECT DISTINCT lf.id, lf.image, lf.name, lf.calo,lf.description,lfs.star AS user_star," +
-            "(SELECT COUNT(1) FROM lw_food_star lfs WHERE lfs.res_partner_id = "+ user_id +" AND lfs.like_flag = 1 " +
-            "AND lfs.food_id = lf.id) AS heart,(SELECT AVG(lfs.star) FROM lw_food_star lfs WHERE lfs.res_partner_id = "+ user_id +" " +
-            "AND lfs.food_id = lf.id) AS star ";
+            `SELECT DISTINCT lf.id, lf.image, lf.name, lf.calo,lf.description,lfs.star AS user_star,
+            (SELECT COUNT(1) FROM lw_food_star lfs WHERE lfs.res_partner_id = ${user_id}  AND lfs.like_flag = 1 
+            AND lfs.food_id = lf.id)::INTEGER AS heart,(SELECT round(AVG(lfs.star)) FROM lw_food_star lfs WHERE lfs.res_partner_id = ${user_id} 
+            AND lfs.food_id = lf.id)::INTEGER AS star `;
         const query =
-            "FROM lw_food lf LEFT JOIN lw_food_star lfs ON lfs.food_id = lf.id INNER JOIN lw_food_lw_menu_rel lflmr " +
-            "ON lflmr.lw_food_id = lf.id INNER JOIN lw_diet ld ON ld.lw_menu_id = lflmr.lw_menu_id INNER JOIN lw_week " +
-            "lw ON lw.id = ld.lw_week_id INNER JOIN lw_menu lm ON lm.id = ld.lw_menu_id WHERE " +
-            "lw.day_of_week = '" + date +"' AND ld.partner_id = " + user_id + " AND lm.code = '" + menu + "' AND lfs.res_partner_id = " + user_id;
+            `FROM lw_food lf LEFT JOIN lw_food_star lfs ON lfs.food_id = lf.id INNER JOIN lw_food_lw_menu_rel lflmr 
+            ON lflmr.lw_food_id = lf.id INNER JOIN lw_diet ld ON ld.lw_menu_id = lflmr.lw_menu_id INNER JOIN lw_week 
+            lw ON lw.id = ld.lw_week_id INNER JOIN lw_menu lm ON lm.id = ld.lw_menu_id WHERE 
+            lw.day_of_week = '${date}' AND ld.partner_id = ${user_id} AND lm.code = '${menu}' AND lfs.res_partner_id = ${user_id}`;
         const count = await this.entityManager.query("SELECT COUNT(DISTINCT(lf.id))" + query);
         const total = parseInt(count[0]["count"]);
         const total_page = Math.ceil(total/limit);
@@ -217,8 +221,8 @@ export class LwFoodRepository extends Repository<LwFood> {
             "SELECT DISTINCT lf.id, lf.image, lf.name, lf.calo,lf.description, " +
             "(select lfs1.star as user_star from lw_food_star lfs1  where lfs1.res_partner_id = " + user_id +" and lfs1.food_id = lf.id)," +
             "(SELECT COUNT(1) FROM lw_food_star lfs WHERE lfs.res_partner_id = "+ user_id +" AND lfs.like_flag = 1 " +
-            "AND lfs.food_id = lf.id) AS heart,(SELECT round(AVG(lfs.star)) FROM lw_food_star lfs WHERE lfs.res_partner_id = "+ user_id +" " +
-            "AND lfs.food_id = lf.id) AS star ";
+            "AND lfs.food_id = lf.id)::INTEGER AS heart,(SELECT round(AVG(lfs.star)) FROM lw_food_star lfs WHERE lfs.res_partner_id = "+ user_id +" " +
+            "AND lfs.food_id = lf.id)::INTEGER AS star ";
         const query =
             "FROM lw_food lf LEFT JOIN lw_food_star lfs ON lfs.food_id = lf.id inner join lw_food_category lfc " +
             "on lfc.food_id = lf.id inner join lw_category lc on lc.code = lfc.category_code ";
@@ -261,7 +265,7 @@ export class LwFoodRepository extends Repository<LwFood> {
     }
 
     public async finishDiet(menuCode: string, dow: string, userId: number): Promise<any> {
-        const diet = await this.entityManager.query(`Select ld.id from lw_diet ld inner join lw_menu lm +
+        const diet = await this.entityManager.query(`Select ld.id from lw_diet ld inner join lw_menu lm
             on lm.id = ld.lw_menu_id inner join lw_week lw on lw.id = ld.lw_week_id where lw.day_of_week = '${dow}'
             and lm.code = '${menuCode}' and ld.partner_id = ${userId}`);
         const diet_id = parseInt(diet[0]["id"]);
